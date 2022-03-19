75d34c581b7887709f4aa4e861eddd6de1aa63a6
Arnel Enero <aenero@gmail.com>
2022-03-19 09:25:36 +0800

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 export { default as colorScheme } from './colorScheme'
 export { default as colorSet } from './colorSet'
-export { default as analogue } from './mappings/analogue'
-export { default as complement } from './mappings/rotation'
-export { default as lightness } from './mappings/lightness'
-export { default as opacity } from './mappings/opacity'
-export { default as triad } from './mappings/triad'
+export { default as normalize } from './normalize'
 export { default as palette } from './palette'
+
+export { default as analogue } from './mappings/analogue'
+export { default as complement } from './mappings/complement'
+export { default as lightness } from './mappings/saturation'
+export { default as opacity } from './mappings/opacity'
+export { default as rotation } from './mappings/rotation'
+export { default as saturation } from './mappings/saturation'
+export { default as triad } from './mappings/triad'
+
+export { default as harmony } from './presets/harmony'
 
 export type {
   ColorRoleMapping,
