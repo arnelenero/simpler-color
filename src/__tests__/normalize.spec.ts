da4a9d8efbabaf04ce88a3491fd1dd865cb49ed8
Arnel Enero <aenero@gmail.com>
2022-03-19 09:24:01 +0800

    Implement color format normalizer

---
+++ src/__tests__/normalize.spec.ts
@@ -0,0 +1,29 @@
+import normalize from '../normalize'
+
+describe('normalize', () => {
+  it('returns hex value if the color is opaque', () => {
+    const opaques = ['blue', '#0000FF', 'rgb(0, 0, 255)', 'hsl(240, 100%, 50%)']
+    opaques.forEach(color => {
+      expect(normalize(color)).toBe('#0000FF')
+    })
+  })
+
+  it('returns rgba value if the color is translucent/transparent', () => {
+    const nonOpaques = [
+      'transparent',
+      '#0000FF88',
+      'rgba(0, 0, 255, 0.5)',
+      'hsla(240, 100%, 100%, 0.5)',
+    ]
+    nonOpaques.forEach(color => {
+      const isRgba = normalize(color).indexOf('rgba(') > -1
+      expect(isRgba).toBe(true)
+    })
+  })
+
+  it('throws if color value is invalid', () => {
+    expect(() => {
+      return normalize('bluish')
+    }).toThrow()
+  })
+})
---
+++ src/normalize.ts
@@ -0,0 +1,20 @@
+import Color from 'color'
+
+/**
+ * Normalizes the color value format into either hex or
+ * rgba
+ *
+ * If the color is translucent, i.e. alpha/opacity value
+ * is less than 1, it is returned as rgba. The 8-digit hex
+ * format is not preferred, to support older browsers.
+ *
+ * @param color
+ * @returns normalized color string
+ * @throws if `color` is not a valid color value
+ */
+export default function normalize(color: string): string {
+  const colorObj = Color(color)
+  const hasAlpha = colorObj.alpha() < 1
+
+  return hasAlpha ? colorObj.rgb().toString() : colorObj.hex()
+}
