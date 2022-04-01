633fde9144431bc6cd7058ece40545a93b30f3e1
Arnel Enero <aenero@gmail.com>
2022-04-01 11:13:59 +0800

    [color engine] Add unit test

--- src/color/__tests__/colorString.spec.ts
+++ src/color/__tests__/colorString.spec.ts
@@ -12,12 +12,17 @@ describe('colorString', () => {
   })
 
   it('returns an rgba() string from RGB object when alpha < 1', () => {
-    const rgb = { r: 255, g: 0, b: 255, a: 0.6 }
-    expect(colorString(rgb)).toBe('rgba(255, 0, 255, 0.6)')
+    const obj = { r: 255, g: 0, b: 255, a: 0.6 }
+    expect(colorString(obj)).toBe('rgba(255, 0, 255, 0.6)')
   })
 
   it('returns an rgba() string from HSL object when alpha < 1', () => {
     const obj = { h: 240, s: 100, l: 50, a: 0.8 }
     expect(colorString(obj)).toBe('rgba(0, 0, 255, 0.8)')
+  })
+
+  it('rounds off r,g,b values in rgba() strings', () => {
+    const obj = { r: 127.5, g: 64.3333, b: 255, a: 0.6 }
+    expect(colorString(obj)).toBe('rgba(128, 64, 255, 0.6)')
   })
 })
